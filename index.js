57ad94393ff7b28510ec19af5eaa7d480b03cf38
Tim <secoif@gmail.com>
2013-04-15 09:49:48 -0400

    Calculate nested offsets correctly.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+"use strict"
+
+var offset = require('offset')
+
 module.exports = function(container, overlayEl) {
 	if (!overlayEl) overlayEl = getOverlay(container)
 	return new Overlay(container, overlayEl)
@@ -13,10 +17,14 @@ function Overlay(container, overlayEl) {
 function show(container, overlayEl) {
 	if (!container || !overlayEl) return
 	overlayEl.style.position = 'absolute'
-	overlayEl.style.top = container.offsetTop + 'px'
+
+  var position = offset(container)
+	overlayEl.style.top = position.top + 'px'
+	overlayEl.style.left = position.left + 'px'
+
 	overlayEl.style.height = container.offsetHeight + 'px'
-	overlayEl.style.left = container.offsetLeft + 'px'
 	overlayEl.style.width = container.offsetWidth + 'px'
+
 	overlayEl.setAttribute('data-overlay', true)
 	overlayEl.style.pointerEvents = 'none';
 	container.appendChild(overlayEl)
